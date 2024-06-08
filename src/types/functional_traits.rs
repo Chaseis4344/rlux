b988b3f7a90882d89f6eee238e8408da1b5ea762
Chaseis4344 <Townsendchase78@yahoo.com>
2024-06-08 16:39:23 -0400

    chore: Comments for clarity

--- src/types/functional_traits.rs
+++ src/types/functional_traits.rs
@@ -1,6 +1,8 @@
 use crate::types::{LiteralType, TokenType};
 
 impl PartialEq for TokenType {
+    /*String comparison is costly and slow, but,
+    we only end up doing it in areas on the dev side, so the cost is acceptable*/
     fn eq(&self, rhs: &Self) -> bool {
         self.to_string() == rhs.to_string()
     }
@@ -10,10 +12,12 @@ impl PartialEq for TokenType {
     }
 }
 
+//Divide literals if possible
 impl std::ops::Div for LiteralType {
     type Output = LiteralType;
     fn div(self, rhs: Self) -> Self::Output {
         match self {
+            ///*if left is number and right is number, multiply them together*/
             Self::Number(left_num) => match rhs {
                 Self::Number(right_num) => LiteralType::Number(left_num / right_num),
                 _ => LiteralType::Number(left_num),
@@ -23,18 +27,31 @@ impl std::ops::Div for LiteralType {
     }
 }
 
+//Add Literals Together if possible
 impl std::ops::Add for LiteralType {
     type Output = LiteralType;
     fn add(self, rhs: Self) -> Self::Output {
         match self {
+            /*
+             * if left is number and right is number, add them together
+             */
             Self::Number(left_num) => match rhs {
                 Self::Number(right_num) => LiteralType::Number(left_num / right_num),
                 _ => LiteralType::Number(left_num),
             },
+
+            /*
+             *if left is String and Right is string, concatonate
+             */
             Self::String(thing1) => match rhs {
                 Self::String(thing2) => LiteralType::String(thing1.to_owned() + &thing2.to_owned()),
                 _ => LiteralType::String(thing1),
             },
+
+            /*
+             *   Return yourself if something unexpected happens.
+             *   This give all programs written in rlux a little more resilience at the cost of predicatbility.
+             */
             _ => self,
         }
     }
@@ -43,6 +60,7 @@ impl std::ops::Add for LiteralType {
 impl std::ops::Mul for LiteralType {
     type Output = LiteralType;
     fn mul(self, rhs: Self) -> Self::Output {
+        //if left is number and right is number, multiply them together
         match self {
             Self::Number(left_num) => match rhs {
                 Self::Number(right_num) => LiteralType::Number(left_num * right_num),
