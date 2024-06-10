b384efa530c56108e20baf6844e465a703b1cda1
Chaseis4344 <Townsendchase78@yahoo.com>
2024-06-09 20:28:49 -0400

    chore: see previous commit?

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -42,20 +42,7 @@ macro_rules! new_literal {
 
 #[test]
 fn test_ast() {
-    let mut calling_expression = new_expression!(new_literal!(LiteralType::Number(45.0)));
-    let mut expression = new_expression!(
-        new_expression!(
-            Token::new(TokenType::Minus, "-".to_string(), None, 1),
-            new_literal!(LiteralType::Number(123.0))
-        ),
-        Token::new(TokenType::Star, "*".to_string(), None, 1),
-        new_expression!(new_literal!(LiteralType::Number(45.67)))
-    );
-
-    assert_eq!(
-        "(* (- 123) (group 45.67))",
-        calling_expression.print(&mut expression)
-    );
+ 
 }
 
 #[test]
