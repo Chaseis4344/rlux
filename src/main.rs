f738bbbdabbc4d6860078f7c0aab3a32441aed5d
Chaseis4344 <Townsendchase78@yahoo.com>
2024-06-11 11:22:39 -0400

    refactor: Remove heavy nesting for readability

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,11 @@ mod types;
 //sends error report to user with specific additonal details
 fn report(line: u32, place: String, message: String) -> Result<i32, Error> {
     eprintln!(" [Line {}]Error{}: {}", line, place, message);
-    Result::Err(Error::new(
+    let return_err = Error::new(
         std::io::ErrorKind::InvalidData,
         format!(" [Line {}]Error{}: {}", line, place, message),
-    ))
+    );
+    Result::Err(return_err)
 }
 
 //Sends an error report to user - semantic sugar
@@ -47,8 +48,6 @@ fn run(source: String) -> Result<i32, Error> {
     let mut interpreter = parser::interpreter::Interpreter::new();
     interpreter.interpret(statements);
 
-    //Print for dev purposes
-
     Result::Ok(65)
 }
 
@@ -62,66 +61,67 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
 //On Startup - Runs source from provided filepath
 pub fn run_file(filepath: String) {
     let source = fs::read_to_string(filepath);
-    match source {
-        Ok(valid_source) => match get_extension_from_filename(&valid_source) {
-            Some(extension) => {
-                //Make sure we have the right extension
-                if extension != "lox" {
-                    println!("Please provide a lox source file.");
-                    exit(65);
-                }
+    //Check source for OS Errors
+    if source.is_err() {
+        let error = source.unwrap_err();
+        eprintln!("File Error: {}", error);
+        exit(65);
+    }
 
-                match run(valid_source) {
-                    Ok(number) => {
-                        exit(number);
-                    }
-                    Err(err) => {
-                        eprintln!("{}", err);
-                        exit(65);
-                    }
-                };
-            }
-            None => {
-                println!("Please provide a lox source file.");
-                exit(65);
-            }
-        },
-        Err(error) => {
-            eprintln!("File Error: {}", error);
+    let valid_source = source.unwrap();
+
+    //Check that we get an extension & its the right one
+    let invalid_extension = get_extension_from_filename(&valid_source);
+    if invalid_extension.is_none() {
+        println!("Please provide a lox source file.");
+        exit(65);
+    }
+
+    let valid_extension = invalid_extension.unwrap();
+    if valid_extension != "lox" {
+        println!("Please provide a lox source file.");
+        exit(65);
+    }
+
+    //Run the code
+    match run(valid_source) {
+        Ok(number) => {
+            exit(number);
+        }
+        Err(err) => {
+            eprintln!("{}", err);
             exit(65);
         }
-    }
+    };
 }
 
 //On startup - Enters Interactive Mode
 pub fn run_prompt() {
     loop {
         let input: &mut String = &mut String::new();
-        //Fancyschmancy stuff
-        //print!("> ");
-        // stdin().read_line(input);
         let matcher = stdin().read_line(input);
-        //Read input & break on error
-        match matcher {
-            Ok(_) => {
-                match run(input.to_string()) {
-                    Ok(number) => {
-                        if number == 0 {
-                            exit(64)
-                        } else {
-                            continue;
-                        }
-                    }
-                    Err(err) => {
-                        eprintln!("{}", err);
-                        exit(65);
-                    }
-                };
-            }
-            Err(err) => {
-                eprintln!("{}", err);
-                exit(65);
-            }
+
+        //Bad Path 1
+        if matcher.is_err() {
+            let err = matcher.unwrap_err();
+            eprintln!("{}", err);
+            exit(65);
+        }
+
+        //Core function of REPL
+        let result = run(input.to_string());
+
+        // Bad Path 2
+        if result.is_err() {
+            let err = result.unwrap_err();
+            eprintln!("{}", err);
+            exit(65);
+        }
+
+        //Check if we exit Normally
+        let number = result.unwrap();
+        if number == 0 {
+            exit(64);
         }
     }
 }
@@ -129,7 +129,6 @@ pub fn run_prompt() {
 fn main() {
     //Collect arguments then run based on number of arguments
     let args: Vec<String> = env::args().collect();
-    //dbg!(args.clone());
 
     match args.len() {
         2 => run_file(args[1].clone()),
