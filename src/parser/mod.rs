92136750b6dddfcd1b61953bf13985da11a8c31d
Chaseis4344 <Townsendchase78@yahoo.com>
2024-08-26 09:07:56 -0400

    rebase: Moved top-level files to Module folders

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,12 +1,69 @@
 //use crate::token::Token;
+<<<<<<< HEAD
 use crate::types::statement::Statement;
 use crate::types::token::Token;
 use crate::types::*;
+=======
+use crate::types::token::Token;
+use crate::types::Expression;
+use crate::types::*;
+use expression::Variable;
+use statement::{ExpressionStatement, PrintStatement, Statement, VariableStatement};
+>>>>>>> 6ee4e7c (chore: Moved top-level files to Module folders)
 
 pub(crate) mod expression;
 pub(crate) mod interpreter;
 pub(crate) mod statement;
 
+<<<<<<< HEAD
+=======
+macro_rules! new_ternary {
+    ($eval:expr, $lhs:expr,  $rhs:expr) => {
+        Expression::Ternary(Box::new(expression::Ternary {
+            evaluator: $eval,
+            left: $lhs,
+            right: $rhs,
+        }))
+    };
+}
+
+macro_rules! new_assignment {
+    ($name:expr, $value:expr) => {
+        Expression::Assignment(Box::new(expression::Assignment {
+            name: $name,
+            value: $value,
+        }))
+    };
+}
+
+macro_rules! new_expression {
+    ($left:expr, $operator:expr,$right:expr) => {
+        Expression::Binary(Box::new(expression::Binary {
+            operator: $operator,
+            left: $left,
+            right: $right,
+        }))
+    };
+    ($operator:expr, $operand:expr) => {
+        Expression::Unary(Box::new(expression::Unary {
+            operator: $operator,
+            operand: $operand,
+        }))
+    };
+    ($expression:expr) => {
+        Expression::Grouping(Box::new(expression::Grouping {
+            expression: $expression,
+        }))
+    };
+}
+
+macro_rules! new_literal {
+    ($value:expr) => {
+        Expression::Literal(Box::new(expression::Literal { value: $value }))
+    };
+}
+
+>>>>>>> 6ee4e7c (chore: Moved top-level files to Module folders)
 pub struct Parser {
     tokens: Vec<Token>,
     current: i32,
@@ -17,6 +74,272 @@ impl Parser {
         Parser { tokens, current }
     }
 
+<<<<<<< HEAD
+=======
+    /*Statement Grammar is Here Down */
+    fn print_statement(&mut self) -> Result<Statement, ParserError> {
+        let expression = self.expression()?;
+        //let expression = pass_up!(value);
+        let _ = self.consume(TokenType::Semicolon, "Expect ';' after value.");
+
+        return Ok(Statement::Print(PrintStatement { expression }));
+    }
+
+    fn expression_statement(&mut self) -> Result<Statement, ParserError> {
+        let expression = self.expression()?;
+
+        let debug = self.consume(TokenType::Semicolon, "Expect ';' after value.");
+        match debug{
+            Ok(ok) =>{}
+            Err(err) => {
+                println!("Expr Err conf");
+                println!("{:?}",err);}
+        } 
+        return Ok(Statement::Expression(ExpressionStatement { expression }));
+    }
+
+    fn if_statement(&mut self) -> Result<Statement, ParserError> {
+        let _ = self.consume(TokenType::LeftParen, "Expected \"(\" after if statement");
+        let condition = self.expression()?;
+        let _ = self.consume(TokenType::RightParen, "Expected \")\" after if statement");
+
+        let then_branch = Box::new(self.statement()?);
+        let else_branch: Option<Statement> = if self.match_token_type(vec![TokenType::Else]) {
+            Some(self.statement()?)
+        } else {
+            None
+        };
+
+        let else_branch = Box::new(else_branch);
+
+        Ok(Statement::If(statement::IfStatement {
+            condition,
+            then_branch,
+            else_branch,
+        }))
+    }
+
+    fn statement(&mut self) -> Result<Statement, ParserError> {
+        if self.match_token_type(vec![TokenType::If]) {
+            self.if_statement()
+        } else if self.match_token_type(vec![TokenType::Print]) {
+            self.print_statement()
+        } else { 
+            println!("Expr Reached");
+            self.expression_statement()
+        }
+    }
+
+    fn variable_decalration(&mut self) -> Result<Statement, ParserError> {
+        let name = self.consume(TokenType::Identifier, "Expected Identifier for Variable")?;
+        //let name = pass_up!(name);
+
+        let initalizer: Expression;
+        if !self.match_token_type(vec![TokenType::Equal]) {
+            
+            return Err(ParserError {
+                source: self.previous(),
+                cause: String::from("Expected '='"),
+            });
+        }
+
+        let result = self.expression();
+        if result.is_err() {
+            return Err(result.unwrap_err());
+        }
+
+        initalizer = result.unwrap();
+        let _ = self.consume(TokenType::Semicolon, "Expexted \";\" following statement");
+
+        let statement = VariableStatement {
+            name,
+            initalizer: Some(initalizer),
+        };
+
+        Ok(Statement::Variable(statement))
+    }
+
+    fn declaration(&mut self) -> Result<Statement, ParserError> {
+        if self.match_token_type(vec![TokenType::Var]) {
+            let result = self.variable_decalration();
+
+            if result.is_err() {
+                let err = result.unwrap_err();
+                eprintln!("{}", err);
+                self.synchronize();
+                return Err(err);
+            }
+
+            return result;
+        } else {
+            let result = self.statement();
+
+            if result.is_err() {
+                let err = result.unwrap_err();
+                eprintln!("{}", err);
+                self.synchronize();
+                return Err(err);
+            }
+
+            return result;
+        }
+    }
+
+    /*Expression Grammar is Here Down */
+    fn expression(&mut self) -> Result<Expression, ParserError> {
+        self.ternary()
+    }
+    fn assignment(&mut self) -> Result<Expression, ParserError> {
+        let expression = self.equality();
+
+        if self.match_token_type(vec![TokenType::Equal]) {
+            let equals = self.previous();
+            let value = self.assignment()?;
+            //let value = pass_up!(value);
+
+            match value.clone() {
+                Expression::Variable(var) => {
+                    let name = var.name;
+                    return Ok(Expression::Assignment(Box::new(expression::Assignment {
+                        name: name,
+                        value: value,
+                    })));
+                }
+
+                _ => {
+                    return Err(ParserError { source: equals, cause: String::from("Bad Variable Expression")});
+                }
+            }
+        }
+
+        expression
+    }
+
+    fn ternary(&mut self) -> Result<Expression, ParserError> {
+        let mut ternary = self.assignment()?;
+
+        while self.match_token_type(vec![TokenType::Question]) {
+            let lhs = self.assignment()?;
+
+            /*Consume ":"/ Enforces Grammar */
+            let _ = self.consume(
+                TokenType::Colon,
+                &(format!("Expected \":\" instead of {}", self.peek())),
+            );
+
+            let rhs = self.assignment()?;
+            ternary = new_ternary!(ternary, lhs, rhs);
+        }
+
+        Ok(ternary)
+    }
+
+    fn equality(&mut self) -> Result<Expression, ParserError> {
+        let mut expression = self.comparison()?;
+        //let mut expression = pass_up!(expression);
+
+        while self.match_token_type(vec![TokenType::BangEqual, TokenType::EqualEqual]) {
+            let operator = self.previous();
+            let right = self.comparison()?;
+            expression = new_expression!(expression, operator, right);
+        }
+
+        Ok(expression)
+    }
+
+    fn comparison(&mut self) -> Result<Expression, ParserError> {
+        let mut expression = self.term()?;
+
+        while self.match_token_type(vec![
+            TokenType::Greater,
+            TokenType::GreaterEqual,
+            TokenType::Less,
+            TokenType::LessEqual,
+        ]) {
+            let operator = self.previous();
+            let right = self.term()?;
+            expression = new_expression!(expression, operator, right);
+        }
+
+        Ok(expression)
+    }
+    fn term(&mut self) -> Result<Expression, ParserError> {
+        let mut expression = self.factor()?;
+
+        while self.match_token_type(vec![TokenType::Minus, TokenType::Plus]) {
+            let operator = self.previous();
+            let right = self.factor()?;
+            expression = new_expression!(expression, operator, right);
+        }
+
+        Ok(expression)
+    }
+
+    fn unary(&mut self) -> Result<Expression, ParserError> {
+        if self.match_token_type(vec![TokenType::Bang, TokenType::Minus]) {
+            let operator = self.previous();
+            let right = self.unary()?;
+            return Ok(new_expression!(operator, right));
+        }
+
+        match self.primary() {
+            Ok(expression) => Ok(expression),
+            Err(_) => Err(Self::error(self.peek(), "Eval error:")),
+        }
+    }
+
+    fn factor(&mut self) -> Result<Expression, ParserError> {
+        let mut expression = self.unary()?;
+
+        while self.match_token_type(vec![TokenType::Slash, TokenType::Star]) {
+            let operator = self.previous();
+            let right = self.unary()?;
+            expression = new_expression!(expression, operator, right);
+        }
+
+        Ok(expression)
+    }
+
+    fn primary(&mut self) -> Result<Expression, ParserError> {
+        if self.match_token_type(vec![
+            TokenType::False,
+            TokenType::True,
+            TokenType::Nil,
+            TokenType::Number,
+            TokenType::String,
+        ]) {
+            /*
+            This will always work bc I am garunteeing that literal types
+            will always have a literal value attatched
+            */
+            let underlying_value = self.previous().literal.unwrap();
+
+            let return_val = match underlying_value {
+                LiteralType::Number(num) => new_literal!(LiteralType::Number(num)),
+                LiteralType::String(string) => new_literal!(LiteralType::String(string)),
+                LiteralType::Boolean(boolean) => {
+                    new_literal!(LiteralType::Boolean(boolean))
+                }
+                LiteralType::Nil => new_literal!(LiteralType::Nil),
+            };
+            return Ok(return_val);
+        } else if self.match_token_type(vec![TokenType::LeftParen]) {
+            let expression = self.expression()?;
+            let _ = self.consume(TokenType::RightParen, "Expect \')\' after expression.");
+            return Ok(new_expression!(expression));
+        } else if self.match_token_type(vec![TokenType::Identifier]) {
+            return Ok(new_expression!(Expression::Variable(Box::new(Variable {
+                name: self.previous()
+            }))));
+        } else {
+            return Err(ParserError {
+                source: self.peek(),
+                cause: String::from("Unexpected Character"),
+            });
+        }
+    }
+
+>>>>>>> 6ee4e7c (chore: Moved top-level files to Module folders)
     fn match_token_type(&mut self, types: Vec<TokenType>) -> bool {
         for token_type in types {
             if self.check(token_type) {
@@ -59,17 +382,25 @@ impl Parser {
         if self.check(type_) {
             Ok(self.advance())
         } else {
+<<<<<<< HEAD
             let token = self.peek();
+=======
+            let mut token = self.peek();
+>>>>>>> 6ee4e7c (chore: Moved top-level files to Module folders)
             Err(Self::error(token, message))
         }
     }
 
     fn error(token: Token, message: &str) -> ParserError {
         let _ = crate::error(token.line, message.to_string());
+<<<<<<< HEAD
         ParserError {
             source: token,
             cause: message.to_string(),
         }
+=======
+        ParserError { source: token, cause: message.to_string() }
+>>>>>>> 6ee4e7c (chore: Moved top-level files to Module folders)
     }
 
     fn synchronize(&mut self) {
