e4533eb49592ad692a2138708986874f1949abc8
Chaseis4344 <townsendchase78@yahoo.com>
2025-06-30 16:35:37 -0400

    BREAKING CHANGE: Add IR Library as wip to project

---
+++ lib-rlux-ir/src/lib.rs
@@ -0,0 +1,18 @@
+mod tests;
+
+#[derive(Debug)]
+enum IR_Block {
+    Jump(Jump),
+    ,
+}
+
+pub fn statement_to_ir_block() -> IR_Block {
+
+}
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+
+    left + right
+}
+
